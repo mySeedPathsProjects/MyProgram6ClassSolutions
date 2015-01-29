966cee34d98eba755dafbd5194c406c43e881202
nate stephens <ndstephens@gmail.com>
2015-01-28 18:33:37 -0700

    adding FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static string FizzBuzz(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Fizz";
+            }else        
+
+            return number.ToString();
+        }
+    }
+}
