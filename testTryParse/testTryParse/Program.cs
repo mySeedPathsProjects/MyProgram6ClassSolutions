44de0085645171ae73487f634578bd08d9270e46
nate stephens <ndstephens@gmail.com>
2015-02-04 16:43:08 -0700

    notes added

---
+++ testTryParse/testTryParse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testTryParse("s");
+            Console.ReadLine();
+        }
+
+        static void testTryParse(string inputString)
+        {
+            int userInt = 0;
+            if (int.TryParse(inputString, out userInt))
+            {
+                Console.WriteLine("works");
+            }
+            Console.WriteLine("nope");
+        }
+    }
+}
